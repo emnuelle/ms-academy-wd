0e25040318a4288c60f6b711761f642986afc655
emnuelle <rm97973@fiap.com.br>
2023-07-22 09:59:01 -0300

    adicionando mais uma section

--- assets/index.js
+++ assets/index.js
@@ -8,6 +8,7 @@ export * from './components/paragrafo'
 // Seções
 export * from './sections/hero-section'
 export * from './sections/vantagens-section'
+export * from './sections/motiv-section'
 
 
 // Páginas
---
+++ assets/sections/motiv-section.js
@@ -0,0 +1,56 @@
+import { LitElement, html, css } from 'lit';
+import { section } from '../styles/section-style';
+
+export class MotivSection extends LitElement {
+    static styles = [
+        section, 
+        css`
+            :host {
+                display: flex;
+                flex-direction: row;
+                flex-wrap: wrap;
+
+                align-items: stretch;
+
+                padding: 0;
+            }
+
+            .container {
+                position: absolute;
+                
+            }
+
+            .img-txt {
+                color: var(--tom-3);
+                font-family: var(--fonte-texto);
+            }
+
+            h1 {
+                position: absolute;
+                align-items: stretch;
+                left: 23px;
+                bottom: 75px;
+
+                color: #042036;
+                font-family: Playfair Display;
+                font-size: 48px;
+                font-style: normal;
+                font-weight: 700;
+                line-height: normal;
+            }
+
+        `
+    ];
+
+    render() {
+        return html`
+        <div class="container">
+                <img src="../../public/img2.png" alt="decole a sua carreira">
+                <div class="img-txt">
+                    <h1>#Quem <br>Se <br>Prepara <br>Sai <br>Na <br>Frente </h1>
+                </div>
+            </div>
+        `;
+    }
+}
+customElements.define('motiv-section', MotivSection);
